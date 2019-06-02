58d41e5319e0834ed7b832ffd861ed63e7d2098f
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-02 16:44:16 +0300

    add new arguments for voting and time in the constructor in quotes.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   ],
   imports: [
     BrowserModule
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-  constructor(public id:number, public theQuote:string, public quoteauthor:string){}
+  constructor(public id:number, public theQuote:string, public quoteauthor:string, public postedDate: Date, public upVotes:number, public downVotes:number){}
 }
