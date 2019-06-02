2276100bf541dce74b5ec32cc2e77fd0d9ba461e
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-02 18:55:11 +0300

    Add pipe that shows the date and time the quote is posted

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,7 +7,7 @@ import {Quote} from "src/app/quote";
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote=new Quote(0,"","",new Date(),0,0);
+  newQuote=new Quote(0,"","","",new Date(),0,0);
 @Output() addQuote=new EventEmitter<Quote>();
 submitQuote(){
   this.addQuote.emit(this.newQuote);
