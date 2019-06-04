79e1535b67c65bd42fa1a8344c12db55a765855c
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-04 10:18:39 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,3 +0,0 @@
-export class Quote {
-  constructor(public id:number,public yourname:string,public theQuote:string, public quoteauthor:string, public postedDate: Date, public upvotes:number, public downvotes:number){}
-}
